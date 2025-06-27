ac423db117920022dc292b1e81f9a6839a7ed5bb
Prachi Negi <prachinegi1001@gmail.com>
2025-06-27 15:41:05 +0530

    Delete route directory

--- route/urlRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createShortUrl, redirectToOriginal } = require('../handler/urlHandler');
-
-router.post('/shorten', createShortUrl);
-router.get('/:shortId', redirectToOriginal);
-
-module.exports = router; 
